f09d05dca0683415a3957142bb1a4db2e81d9982
Dan <elliottdanielp@gmail.com>
2022-06-27 14:11:30 -0400

    Adding PyPoll.py, Resouces folder, analysis folder and election_analysis.py

---
+++ PyPoll.py
@@ -0,0 +1,76 @@
+# the data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on populate vote. 
+
+# import csv
+
+# # Assign variable for file to load 
+# file_to_load = 'Resources/election_results.csv'
+
+# # Open election results and read the file
+# with open(file_to_load) as election_data:
+
+# # To do: perform analysis.
+#     print(election_data)
+
+
+# import csv
+# import os
+
+# # Assign a variable for the file to load and the path.
+# file_to_load = os.path.join("Resources", "election_results.csv")
+
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+
+#     # Print the file object.
+#      print(election_data)
+
+
+# import csv
+# import os
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Use the open statement to open the file as a text file. 
+# # outfile = open(file_to_save, "w")
+
+# # Use the with statement to open file as a text file.
+# with open(file_to_save, "w") as txt_file:
+
+#     # Write data to the file
+#     txt_file.write("Counties in the Election\n--------------\nArapahoe\nDenver\nJefferson")
+    
+# # Close the file if you use the open statment. Do not need close statment when using WITH statment to open file.
+# # outfile.close()
+
+
+
+# Add our dependencies.
+import csv
+from email import header
+import os
+
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Read the file object with the reader function. 
+    file_reader = csv.reader(election_data)
+
+    # Print each row in the CSV file. 
+    # for row in file_reader:
+    #     print(row)
+
+    # Print the header row. 
+    headers = next(file_reader)
+    print(headers)
